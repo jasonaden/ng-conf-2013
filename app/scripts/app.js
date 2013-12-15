2a0541d0af69e1d1b14bbe28da75333e8b412901
Jason Aden <jasonaden@gmail.com>
2013-12-15 10:20:45 -0500

    Clean out Jumbotron and format HTML and CSS with Webstorm default formatting.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('ngConf2013App', [
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
