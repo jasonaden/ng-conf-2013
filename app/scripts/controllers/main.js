97feed3307f716747b23d49f7de4f67e74e33271
Jason Aden <jasonaden@gmail.com>
2013-12-29 08:24:53 -0500

    Update with ng-change.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,8 @@
 angular.module('ngConf2013App')
     .controller('MainCtrl', function ($scope) {
         this.myVariable = 'Model Value!';
+        this.changes = 0;
+        this.itRan = function () {
+            this.changes++;
+        }
     });
